9d2e781776ec71f66b9e7a51c0643d6c6361d900
Bernardo Ribeiro <bernardo.ribeiro98@gmail.com>
2021-03-08 12:04:58 +0000

    Syncronyzed DateUtilsTest.java with remote

--- src/test/java/jpass/util/DateUtilsTest.java
+++ src/test/java/jpass/util/DateUtilsTest.java
@@ -11,54 +11,52 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class DateUtilsTest {
-	
-	
-	/**
-	 * formatIsoDateTime
-	 * 
-	 * String
-	 * valida | Invalida
-	 * 
-	 * Formater
-	 * dar formater válido, dar formater inválido
-	 */
-	
-	@Test
-	public void shouldHandleValidDateTime() {
-		String validDateTime = "2012-12-03T10:15:30";
-		String result = DateUtils.formatIsoDateTime(validDateTime, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-		Assert.assertEquals(validDateTime, DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(LocalDateTime.parse(validDateTime, DateTimeFormatter.ISO_LOCAL_DATE_TIME).truncatedTo(ChronoUnit.SECONDS)));
-		
 
-	}
-	
-	
-	@Test
-	public void shouldHandleInvalidDateTime() {
-		String invalidDateTime = "2012-12-03";
-		String result = DateUtils.formatIsoDateTime(invalidDateTime, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-		Assert.assertEquals(DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(LocalDateTime.ofInstant(Instant.ofEpochMilli(0), ZoneId.systemDefault())), result);
-		
 
-	}
-	
-	@Test
-	public void shouldHandleNullDateTime() {
-		String nullDateTime = null;
-		String result = DateUtils.formatIsoDateTime(nullDateTime, DateTimeFormatter.ISO_LOCAL_DATE_TIME);		
-		Assert.assertEquals(DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(LocalDateTime.ofInstant(Instant.ofEpochMilli(0), ZoneId.systemDefault())), result);
-		
+    /**
+     * formatIsoDateTime
+     *
+     * String
+     * valida | Invalida
+     *
+     * Formater
+     * dar formater válido, dar formater inválido
+     */
 
-	}
-	
-	@Test
-	public void shouldHandleNullFormater() {
-		String validDateTime = "2012-12-03T10:15:30";
-		String result = DateUtils.formatIsoDateTime(validDateTime, null);
-		Assert.assertEquals(validDateTime, DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(LocalDateTime.parse(validDateTime, DateTimeFormatter.ISO_LOCAL_DATE_TIME).truncatedTo(ChronoUnit.SECONDS)));
-		
-	}
-	
-	
-	
+    @Test
+    public void shouldHandleValidDateTime() {
+        String validDateTime = "2012-12-03T10:15:30";
+        String result = DateUtils.formatIsoDateTime(validDateTime, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        Assert.assertEquals(validDateTime, DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(LocalDateTime.parse(validDateTime, DateTimeFormatter.ISO_LOCAL_DATE_TIME).truncatedTo(ChronoUnit.SECONDS)));
+
+
+    }
+
+
+    @Test
+    public void shouldHandleInvalidDateTime() {
+        String invalidDateTime = "2012-12-03";
+        String result = DateUtils.formatIsoDateTime(invalidDateTime, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        Assert.assertEquals(DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(LocalDateTime.ofInstant(Instant.ofEpochMilli(0), ZoneId.systemDefault())), result);
+
+
+    }
+
+    @Test
+    public void shouldHandleNullDateTime() {
+        String nullDateTime = null;
+        String result = DateUtils.formatIsoDateTime(nullDateTime, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        Assert.assertEquals(DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(LocalDateTime.ofInstant(Instant.ofEpochMilli(0), ZoneId.systemDefault())), result);
+
+
+    }
+
+    @Test
+    public void shouldHandleNullFormater() {
+        String validDateTime = "2012-12-03T10:15:30";
+        String result = DateUtils.formatIsoDateTime(validDateTime, null);
+        Assert.assertEquals(validDateTime, DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(LocalDateTime.parse(validDateTime, DateTimeFormatter.ISO_LOCAL_DATE_TIME).truncatedTo(ChronoUnit.SECONDS)));
+
+    }
+
 }
